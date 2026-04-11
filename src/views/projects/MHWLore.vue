192dff1ee6fd27523b2a6d5c70035244e35ce2a2
blanchard960 <elyne.derocles@laposte.net>
2026-04-11 21:41:56 +0200

    MHWLore project add techno

--- src/views/projects/MHWLore.vue
+++ src/views/projects/MHWLore.vue
@@ -3,8 +3,7 @@
         <div class="flex-row">
             <p>!!! There is two projects here !!!</p>
         </div>
-        <p class="technos"><strong>Technos & outils : </strong>Kotlin - Flutter - Dart - gitHub - gitHub Projects -
-            Trello</p>
+        <p class="technos"><strong>Technos & outils : </strong>Kotlin - Flutter - Dart - gitHub - gitHub Projects - Figma - Trello</p>
         <p class="technos"><strong>Keywords : </strong>Team work - Mobile App</p>
     </main>
 </template>
